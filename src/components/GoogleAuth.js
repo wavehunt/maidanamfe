64a3d86eb71e46b9632520f463a8b08427e27fc1
Sankar <sankar@Sankars-MacBook-Air.local>
2022-01-25 08:40:01 +0530

    use package

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,145 +1,55 @@
-import React from "react";
+import React from 'react';
+import { useState,useEffect } from 'react';
+import {GoogleLogin,GoogleLogout} from 'react-google-login';
 
-class GoogleAuth extends React.Component {
+const GoogleAuth = () => {
+    const [isSignedIn,setIsSignedIn] = useState(null);
 
-    //State for sign-in status
-    state = {isSignedIn:null};
-    
-    //load and initialize the gapi on component load
-    componentDidMount () {
-            
-        const gapiLoadInit = () => {            
-            let script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.src = 'https://apis.google.com/js/api.js'
-            script.onload = () => {
-              window.gapi.load('client:auth2', async () => {
-                console.log('gapi loaded');
-                alert('gapi loaded');
-                const initApi = () => {
-                   if (!window.gapi.client) {
-                     console.log('client not present');
-                     alert('client not present');
-                     return;
-                   }
-                   if (!window.gapi.auth2.getAuthInstance()) {
-                    window.gapi.client.init({
-                        'clientId': '515718912201-5o9h09qa20j5vsl1t7gunc9917ad6kck.apps.googleusercontent.com',
-                        'scope': 'profile',
-                        
-                        'ux_mode': 'redirect',
-                        'redirect_uri':'https://maidanamreact.herokuapp.com'
-                        //'redirect_uri':'http://localhost:3000'
-                        
-                        })
-                        .then((result)=> {
-                          console.log("gapi init done");
-                          alert("gapi init done");
-                          console.log("result: ", result);
-    
-                          this.auth = window.gapi.auth2.getAuthInstance();                    
-                            //setup listener for sign-in changes
-                            this.auth.isSignedIn.listen(this.handleAuthChange);
-                
-                            //handle initial state
-                            this.handleAuthChange(this.auth.isSignedIn.get());
-                            console.log(this.auth);
-                        })
-                        .catch(e=> {
-                          console.log("error in init ", e);
-                        })
-
-                   } else {
-                    this.auth = window.gapi.auth2.getAuthInstance();                    
-                    //setup listener for sign-in changes
-                    this.auth.isSignedIn.listen(this.handleAuthChange);
-        
-                    //handle initial state
-                    this.handleAuthChange(this.auth.isSignedIn.get());
-                    console.log(this.auth);
-                   }
-                   
-                }
-                setTimeout(initApi,100);
-              })
-
-
-            }
-
-        document.getElementsByTagName('head')[0].appendChild(script)
-          }
-      
-          gapiLoadInit();
-
-    }
-    
-        
-    /*
-    useEffect(
-        ()=> window.gapi.load('client:auth2', initClient),
-        []
-    );
-    */
-
-    //respond to sign-in changes
-    handleAuthChange = (userLoggedIn) => {
-        alert (userLoggedIn);
-
-        if(userLoggedIn) {
-            //setIsSignedIn(true);
-            this.setState({isSignedIn:true})
-        } else {
-            //setIsSignedIn(false);
-            this.setState({isSignedIn:false})
+    const responseGoogle = (resp) => {
+        //console.log('response: ', resp)
+        if (resp.error) {
+            setIsSignedIn(false);
+            return console.log(resp.error);
         }
+
+        console.log(resp);
+        setIsSignedIn(true);
+         
     }
 
-    onSignInClick = () => {
-        //const authObj = auth=='null'?'yes':'no'
-        console.log(this);
-        alert('çlicked: ' + this.auth );
-        //console.log('auth: ', auth)
-        if(this && this.auth) {
-            console.log(this.auth.signIn());
-        }
-    }
-    onSignOutClick = () => {
-        console.log(this);
-        if(this && this.auth) {
-            this.auth.signOut();
-        }
-        //window.gapi.auth2.getAuthInstance().signOut();
+    const logout = () => {
+        console.log('user logged out');
+        setIsSignedIn(false);
     }
 
-    renderButton () {
-        return this.state.isSignedIn ? 
-            <button 
-                    
-                    onClick={this.onSignOutClick} 
-                    className='ui button negative'
-            >
-                Logout
-            </button> :
-            <button
-                    
-                    onTouchStart={this.onSignInClick} 
-                    onClick={this.onSignInClick} 
-                    className='ui button link-fmt'
-                    //style={{backgroundColor:'#4285F4',color:'#FFFFFF'}}
-            >
-                <i className='google icon'/>
-                Logina
-            </button>
-    }
-
-    render() {
+    const renderButton = () => {
         return (
-            <div>
-                {this.renderButton()}
-            </div>
+            !isSignedIn ?
+            <GoogleLogin
+                clientId='515718912201-5o9h09qa20j5vsl1t7gunc9917ad6kck.apps.googleusercontent.com'
+                buttonText='Login'
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy='single_host_origin'
+                isSignedIn={true}
+            /> :
+            <GoogleLogout
+                clientId='515718912201-5o9h09qa20j5vsl1t7gunc9917ad6kck.apps.googleusercontent.com'
+                buttonText='Logout'
+                onLogoutSuccess={logout}
+            />
         )
-    }
-    
+    };
+
+    return (
+        <div>
+            {renderButton()}
+        </div>
+    )
+
+
 }
 
 export default GoogleAuth;
+
+
---
+++ src/components/GoogleAuthCl.js
@@ -0,0 +1,152 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+
+    //State for sign-in status
+    state = {isSignedIn:null};
+    
+    //load and initialize the gapi on component load
+    componentDidMount () {
+            
+        const gapiLoadInit = () => {            
+            let script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.src = 'https://apis.google.com/js/api.js'
+            script.onload = () => {
+              window.gapi.load('client:auth2', async () => {
+                console.log('gapi loaded');
+                alert('gapi loaded');
+                const initApi = async () => {
+                   if (!window.gapi.client) {
+                     console.log('client not present');
+                     alert('client not present');
+                     return;
+                   }
+                   if (!window.gapi.auth2.getAuthInstance()) {
+                    await window.gapi.client.init({
+                        'clientId': '515718912201-5o9h09qa20j5vsl1t7gunc9917ad6kck.apps.googleusercontent.com',
+                        'scope': 'profile',
+                        
+                        'ux_mode': 'redirect',
+                        //'redirect_uri':'https://maidanamreact.herokuapp.com'
+                        'redirect_uri':'http://localhost:3000'
+                        
+                        });
+                        //gauth.then((result)=> {
+                          console.log("gapi init done");
+                          alert("gapi init done");
+                          //console.log("result: ", result);
+    
+                          this.auth = window.gapi.auth2.getAuthInstance();                    
+                          console.log(this.auth);
+                          //this.auth = result;
+                          //handle initial state
+                        
+                          this.handleAuthChange(this.auth.isSignedIn.get());
+                            //setup listener for sign-in changes
+                            //this.auth.disconnect();
+                            this.auth.isSignedIn.listen(this.handleAuthChange);
+                
+                            
+                            
+                        
+                        //.catch(e=> {
+                        //    ,e=> {
+                        //      console.log("error in init ", e);
+                        //})
+
+                   }  
+                   else {
+                    this.auth = window.gapi.auth2.getAuthInstance();                    
+                    //setup listener for sign-in changes
+                    this.auth.isSignedIn.listen(this.handleAuthChange);
+        
+                    //handle initial state
+                    this.handleAuthChange(this.auth.isSignedIn.get());
+                    console.log(this.auth);
+                   }
+                   
+                }
+                setTimeout(initApi,100);
+              })
+
+
+            }
+
+        document.getElementsByTagName('head')[0].appendChild(script)
+          }
+      
+          gapiLoadInit();
+
+    }
+    
+        
+    /*
+    useEffect(
+        ()=> window.gapi.load('client:auth2', initClient),
+        []
+    );
+    */
+
+    //respond to sign-in changes
+    handleAuthChange = (userLoggedIn) => {
+        alert (userLoggedIn);
+        console.log(this.auth.currentUser.get());
+        if(userLoggedIn) {
+            //setIsSignedIn(true);
+            this.setState({isSignedIn:true})
+        } else {
+            //setIsSignedIn(false);
+            this.setState({isSignedIn:false})
+        }
+    }
+
+    onSignInClick = () => {
+        //const authObj = auth=='null'?'yes':'no'
+        console.log(this);
+        alert('çlicked: ' + this.auth );
+        //console.log('auth: ', auth)
+        if(this && this.auth) {
+            console.log(this.auth.signIn());
+        }
+    }
+    onSignOutClick = () => {
+        console.log(this);
+        if(this && this.auth) {
+            this.auth.signOut();
+        }
+        //window.gapi.auth2.getAuthInstance().signOut();
+    }
+
+    renderButton () {
+        return this.state.isSignedIn ? 
+            <button 
+                    
+                    onClick={this.onSignOutClick} 
+                    className='ui button negative'
+            >
+                Logout
+            </button> :
+            <button
+                    
+                    onTouchStart={this.onSignInClick} 
+                    onClick={this.onSignInClick} 
+                    className='ui button link-fmt'
+                    //style={{backgroundColor:'#4285F4',color:'#FFFFFF'}}
+            >
+                <i className='google icon'/>
+                Logina
+            </button>
+    }
+
+    render() {
+        return (
+            <div>
+                {this.renderButton()}
+            </div>
+        )
+    }
+    
+}
+
+export default GoogleAuth;
