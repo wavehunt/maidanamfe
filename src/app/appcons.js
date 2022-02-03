9fa83b20fd922af0353fd0041a6cfdcb6daa471e
Sankar <sankar@Sankars-MacBook-Air.local>
2022-02-03 12:10:37 +0530

    https changes

--- src/app/appcons.js
+++ src/app/appcons.js
@@ -6,6 +6,6 @@ export const REDIRECT_URL = 'http://localhost:3000/auth';
 
 //Heroku
 export const API_URL = 'https://maidanambe.herokuapp.com/';
-export const REDIRECT_URL = 'http://maidanamreact.herokuapp.com/auth';
+export const REDIRECT_URL = 'https://maidanamreact.herokuapp.com/auth';
 
 export const GOOGLE_CLIENT_ID = '515718912201-5o9h09qa20j5vsl1t7gunc9917ad6kck.apps.googleusercontent.com'
